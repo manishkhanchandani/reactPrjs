bc61c83301441aebef4520ebb8fec7e00c7d745e
Manish Khanchandani <manishkk74@gmail.com>
2016-11-22 17:09:21 -0800

    src

---
+++ src/js/components/App.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+var App = React.createClass({
+  render: function() {
+    return(
+      <div>
+        MY APP
+      </div>
+    )
+  }
+});
+
+
+module.exports = App;
