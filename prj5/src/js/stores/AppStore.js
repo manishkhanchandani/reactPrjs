aef917d5c50c747fbffaf60584c1662beb31608f
Manish Khanchandani <manishkk74@gmail.com>
2016-11-22 17:55:13 -0800

    prj5

--- prj5/src/js/stores/AppStore.js
+++ prj5/src/js/stores/AppStore.js
@@ -6,9 +6,16 @@ var AppAPI = require('../utils/AppAPI.js');
 
 var CHANGE_EVENT = 'change';
 
-var _items = [];
+var _contacts = [];
 
 var AppStore = assign({}, EventEmitter.prototype, {
+  saveContact: function(contact) {
+    _contacts.push(contact);
+    console.log('14. contacts is ', _contacts);
+  },
+  getContacts: function() {
+    return _contacts;
+  },
 	emitChange: function(){
 		this.emit(CHANGE_EVENT);
 	},
@@ -24,7 +31,15 @@ AppDispatcher.register(function(payload){
 	var action = payload.action;
 
 	switch(action.actionType){
-		
+    case AppConstants.SAVE_CONTACT:
+      console.log('Saving Contacts ....: ', action);
+      
+      //Store save
+      AppStore.saveContact(action.contact);
+      
+      //Emit Change
+      AppStore.emit(CHANGE_EVENT);
+      break;
 	}
 
 	return true;
